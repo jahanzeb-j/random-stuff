262c766d187340a0da0c685667c092652313386e
Jahanzeb Jabbar <jahanzeb.me4u@gmail.com>
2021-05-30 18:00:17 +0800

    python code for scraping website data using request
    -checkCert.py have python code for scraping website data using request.
    -codes.txt file have random practice codes

---
+++ checkCert.py
@@ -0,0 +1,29 @@
+import requests
+
+# i = 14038
+# while True:
+	# addr = 'https://prvs.sindhpolice.gov.pk/verification/CheckCertificate?id='+str(i)
+	# page = requests.get(addr)
+	# re = page.text.find('ASIF')
+	# print(re)
+	# print(i)
+	# if(i >14200):
+		# print(i)
+		# print(re)
+		# break
+	
+	# i = i+1
+
+i = 14000
+while True:
+	addr = 'https://prvs.sindhpolice.gov.pk/verification/CheckCertificate?id='+str(i)
+	page = requests.get(addr)
+	name = page.text[9074:9090]
+	country =  page.text[10278:10320] 
+	print(str(i)+','+name+','+country)
+	if(i >14500):
+		print(i)
+		break
+	
+	i = i+1
+
